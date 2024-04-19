0f336c9602c6b238ac5d4401a6c17b323bde2920
Vivek Chaturvedi <stayalive1989@gmail.com>
2024-04-19 15:28:09 +0530

    Fixed the springboot-graphQL dependency issue.

--- src/main/java/com/example/query/Query.java
+++ src/main/java/com/example/query/Query.java
@@ -1,17 +1,22 @@
 package com.example.query;
 
+import graphql.kickstart.tools.GraphQLQueryResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
-import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 
 @Component
 public class Query implements GraphQLQueryResolver {
 
+	private static final Logger logger = LoggerFactory.getLogger(Query.class);
+
 	public String firstQuery () {
-		return "First Query";
+		logger.info("{}#firstQuery() is called !", this.getClass().getSimpleName());
+		return "First Query Data";
 	}
 	
 	public String secondQuery () {
-		return "Second Query";
+		logger.info("{}#secondQuery() is called !", this.getClass().getSimpleName());
+		return "Second Query Data";
 	}
 }
