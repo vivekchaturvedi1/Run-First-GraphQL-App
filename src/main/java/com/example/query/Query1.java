f8626fb57a83a4c72175ec77a3cdc91745655d5d
Vivek Chaturvedi <Vivek.Chaturvedi@sunlife.com>
2024-04-22 22:56:24 +0530

    Initial Commit for Project GraphQL-With-Spring-Boot

--- src/main/java/com/example/query/Query1.java
+++ src/main/java/com/example/query/Query1.java
@@ -1,9 +1,13 @@
 package com.example.query;
 
+import com.example.request.SampleRequest;
+import com.example.response.SampleResponse;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import static org.apache.commons.lang3.StringUtils.SPACE;
 
 @Component
 public class Query1 implements GraphQLQueryResolver {
@@ -19,4 +23,28 @@ public class Query1 implements GraphQLQueryResolver {
 		logger.info("{}#secondQuery() is called !", this.getClass().getSimpleName());
 		return "Second Query Data";
 	}
+
+	public String fullName (String firstName, String lastName) {
+		logger.info("{}#fullName() is called !", this.getClass().getSimpleName());
+		return firstName + SPACE + lastName;
+	}
+
+	public String fullNameWithAge (String firstName, String secondName, Integer age) {
+		logger.info("{}#fullNameWithAge() is called with Age: {}!", this.getClass().getSimpleName(), age);
+		return firstName + SPACE + secondName + " Age is " + age.toString();
+	}
+
+	public String fullNameWithJsonInput (SampleRequest request) {
+		logger.info("{}#fullNameWithJsonInput() is called  | Input Request : {}", this.getClass().getSimpleName(), request);
+		return request.getFirstName() + SPACE + request.getLastName();
+	}
+
+	public SampleResponse fullNameWithJsonInputAndModelResp (SampleRequest request) {
+		logger.info("{}#fullNameWithJsonInputAndModelResp() is called  | Input Request : {}", this.getClass().getSimpleName(), request);
+		SampleResponse response = new SampleResponse();
+		response.setResponse(request.getFirstName()  + SPACE + request.getLastName());
+		logger.info("Inside {}#fullNameWithJsonInputAndModelResp() method | Output Response : {}",this.getClass().getSimpleName(), response);
+		return response;
+	}
+
 }
